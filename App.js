5690ebaef2ff8d6fdf1e436152f5edf1e0589acf
Michael Pusterhofer <michael.pusterhofer@ams.com>
2022-02-03 21:54:00 +0100

    make interactive

--- App.js
+++ App.js
@@ -5,38 +5,41 @@ export default {
         <div id ="inputs">
         <form id="parameter-form">
         <fieldset>
-            <p><label>$N_D [1/cm^3]$</label> <input v-model.number="ND" type="number" @change="update"></p>
-            <p><label>$N_A [1/cm^3]$</label> <input type="number" id="NA" value=1e15></p>
+            <p>
+                <label>$N_D [1/cm^3]$</label>
+                <input v-model.number="ND" type="number" @change="update">
+            </p>
+            <p>
+                <label>$N_A [1/cm^3]$</label>
+                <input v-model.number="NA" type="number" @change="update">
+            </p>
         </fieldset>
         <fieldset>
-            <p><label>$\epsilon [\epsilon_0]$</label> <input type="number" id="epsilon" value=12></p>
-            <p><label>$L_p [\mu m]$</label> <input type="number" id="LP" value=0.352></p>
-            <p><label>$L_n [\mu m]$</label> <input type="number" id="LN" value=0.591></p>
-            <p><label>$N_c(300) [1/cm^3]$</label> <input type="number" id="Nc" value=2.78E19></p>
-            <p><label>$N_v(300) [1/cm^3]$</label> <input type="number" id="Nv" value=9.84E18></p>
-            <p><label>$E_g [eV]$</label> <input type="number" id="Eg" value=1.1205192307692307></p>
+            <p><label>$\epsilon [\epsilon_0]$</label> <input type="number" v-model.number="epsilon" @change="update"></p>
+            <p><label>$N_c(300) [1/cm^3]$</label> <input type="number" v-model.number="Nc300" @change="update"></p>
+            <p><label>$N_v(300) [1/cm^3]$</label> <input type="number" v-model.number="Nv300" @change="update"></p>
+            <p><label>$E_g [eV]$</label> <input type="number"  v-model.number="Eg" @change="update"></p>
         </fieldset>
         <fieldset>
-            <p><label>$T [K]$</label> <input type="number" id="T" value=300></p>
-            <p><label>$V_{min} [V]$</label> <input type="number" id="Vmin" value=-1></p>
-            <p><label>$V_{max} [V]$</label> <input type="number" id="Vmax" value=1></p>
+            <p><label>$T [K]$</label> <input type="number" v-model.number="T" @change="update"></p>
+            <p><label>$V_{min} [V]$</label> <input type="number" v-model.number="Vmin" @change="update"></p>
         </fieldset>
         <p><button type="button" >Replot</button></p>
     </form>
         </div>
         <div id="plots">
             <div id="plot_Cj" style="width:400px;height:300px"></div>
-         </div>
+        </div>
   `,
     data: function () {
         return {
             plt_data: [],
             Vmin: -2,
-            Eg: 1.13,
-            Nc300: 1,
-            Nv300: 1,
-            NA: 1e13,
-            ND: 1e13,
+            Eg: 1.12,
+            Nc300: 2.78E19,
+            Nv300: 9.84E18,
+            NA: 1e15,
+            ND: 1e15,
             epsilon: 12,
             T: 300,
             n: 200
