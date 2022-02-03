6d21750604fac298ff27ef360b6eb278bafac046
Michael Pusterhofer <michael.pusterhofer@ams.com>
2022-02-03 20:48:11 +0100

    plot something

--- App.js
+++ App.js
@@ -2,11 +2,11 @@
 export default {
     name:"App",
     template: `
-        <div id="p1">test</div>
+        <div id="p1" width="400px" height="400px"></div>
     `,
     methods: {
         plot() {
-            $.plot("#p1",[])
+            $.plot("#p1",[[0,0],[1,1]])
         }
     },
     mounted() {
