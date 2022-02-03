b5e4d3f5f8f9823bf9bcea5ffaad379f0afc667c
Michael Pusterhofer <michael.pusterhofer@ams.com>
2022-02-03 21:34:39 +0100

    add inputs

--- App.js
+++ App.js
@@ -2,6 +2,28 @@
 export default {
     name: "App",
     template: `
+        <div id ="inputs">
+        <form id="parameter-form">
+        <fieldset>
+            <p><label>$N_D [1/cm^3]$</label> <input v-model.number="ND" type="number"></p>
+            <p><label>$N_A [1/cm^3]$</label> <input type="number" id="NA" value=1e15></p>
+        </fieldset>
+        <fieldset>
+            <p><label>$\epsilon [\epsilon_0]$</label> <input type="number" id="epsilon" value=12></p>
+            <p><label>$L_p [\mu m]$</label> <input type="number" id="LP" value=0.352></p>
+            <p><label>$L_n [\mu m]$</label> <input type="number" id="LN" value=0.591></p>
+            <p><label>$N_c(300) [1/cm^3]$</label> <input type="number" id="Nc" value=2.78E19></p>
+            <p><label>$N_v(300) [1/cm^3]$</label> <input type="number" id="Nv" value=9.84E18></p>
+            <p><label>$E_g [eV]$</label> <input type="number" id="Eg" value=1.1205192307692307></p>
+        </fieldset>
+        <fieldset>
+            <p><label>$T [K]$</label> <input type="number" id="T" value=300></p>
+            <p><label>$V_{min} [V]$</label> <input type="number" id="Vmin" value=-1></p>
+            <p><label>$V_{max} [V]$</label> <input type="number" id="Vmax" value=1></p>
+        </fieldset>
+        <p><button type="button" onclick="plotn()">Replot</button></p>
+    </form>
+        </div>
         <div id="plots">
             <div id="plot_Cj" style="width:400px;height:300px"></div>
          </div>
